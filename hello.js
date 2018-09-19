21255778b0c4460404038ad81fceef044d422ee3
Charlie van Rantwijk <charlievanrantwijk@gmail.com>
2018-09-19 14:32:51 +0200

    Implement ugly .then() chain to anger NodeJs devs

--- hello.js
+++ hello.js
@@ -1,6 +1,6 @@
 var indy = require('indy-sdk')
 
-let name = "Charlie";
+let name = "Henkie";
 let currentHandle = 0;
 
 let createWallet = function(){
@@ -11,24 +11,51 @@ let openWallet = function(){
     return indy.openWallet({"id":name},{"key":"password"});
 }
 
-let exportWallet = function(){
-    console.log("Current handle: ");
-    console.log(currentHandle);
-    return indy.exportWallet(1, {"path":"/home/charlie/Desktop/wallets/" + name + ".wallet", "key":"password"});
+let closeWallet = function(){
+    return indy.closeWallet(currentHandle);
 }
 
+let exportWallet = function(){
+    return indy.exportWallet(currentHandle, {"path":"/home/charlie/Desktop/wallets/" + name + ".wallet", "key":"password"});
+}
+
+// create wallet
 createWallet().then(function(result){
     console.log("Creating wallet...");
-    console.log(result);
-});
+    if(result==null){
+        console.log("Success!");
+    }else{
+        console.log("ERROR: " + result);
+    }
 
-openWallet().then(function(result){
-    currentHandle=result;
-    console.log("Opening wallet...");
-    console.log(result);
-});
+    // open wallet when created
+    openWallet().then(function(result){
+        currentHandle=result;
+        console.log("Opening wallet...");
+        if(result==1){
+            console.log("Success!");
+        }else{
+            console.log("ERROR: " + result);
+        }
 
-exportWallet().then(function(result){
-    console.log("Exporting wallet...");
-    console.log(result);
+        // export wallet when opened
+        exportWallet().then(function(result){
+            console.log("Exporting " + name + "'s wallet...");
+            if(result==null){
+                console.log("Success!");
+            }else{
+                console.log("ERROR: " + result);
+            }
+
+            // close wallet after export
+            closeWallet().then(function(result){
+                console.log("Closing " + name + "'s wallet");
+                if(result==null){
+                    console.log("Success!");
+                }else{
+                    console.log("ERROR: " + result);
+                }
+            });
+        });
+    });
 });
